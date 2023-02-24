6174743f609136393710fa43bc6b187e9fd6ad84
kriskris79 <krzysztofkakolewski@googlemail.com>
2023-02-24 23:26:10 +0000

    navbar

---
+++ website/src/components/Grid.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+// import './App.css';
+
+function App() {
+    return (
+        <div className="container">
+            <div className="row row-1">
+                <div className="col-12">
+                    {/* full width menu here */}
+                </div>
+            </div>
+            <div className="row row-2">
+                <div className="col-6">
+                    {/* left column content here */}
+                </div>
+                <div className="col-6">
+                    {/* right column content here */}
+                </div>
+            </div>
+            <div className="row row-3">
+                <div className="col-12">
+                    {/* full width content here */}
+                </div>
+            </div>
+            <div className="row row-4">
+                <div className="col-12">
+                    {/* footer content here */}
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
